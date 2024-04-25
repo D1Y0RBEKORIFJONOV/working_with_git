cd9c9a63bd3689d0ff2e79b491f07597a9c2d099
Diyorbek <D1Y0RBEKORIFJONOV>
2024-04-25 19:20:42 +0500

    appendUsers

--- git/git.go
+++ git/git.go
@@ -12,12 +12,3 @@ func GetUserName() (string,error) {
 	}
 	return string(str),err
 }
-
-func GetUserEmail() (string,error) {
-	cmd := exec.Command("git", "config", "user.email")
-	str, err := cmd.Output()
-	if err != nil {
-		return "",err
-	}
-	return string(str),err
-}
