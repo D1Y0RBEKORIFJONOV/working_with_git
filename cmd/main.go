944358535b492983aed2314dde1d1d13228158ac
Diyorbek <D1Y0RBEKORIFJONOV>
2024-04-25 19:55:44 +0500

    appendUsers

--- cmd/main.go
+++ cmd/main.go
@@ -1,41 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 
 	"github.com/D1Y0RBEKORIFJONOV/working_with_git.git/git"
 )
 
-const PATHS  = "/home/diyorbek/go/src/working_with_git/file.txt"
-func PrintToFileData(fileName string,data string) error {
-	file,err  := os.OpenFile(fileName,os.O_APPEND|os.O_WRONLY,0666)
+const PATHS = "/home/diyorbek/go/src/working_with_git/file.txt"
+
+func PrintToFileData(fileName string, data string) error {
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0666)
 	if err != nil {
 		return err
 	}
 	file.Write([]byte(data))
 
-	cmd := exec.Command("git","add",".")
+	cmd := exec.Command("git", "add", ".")
 	cmd.Output()
-	cmd = exec.Command("git","commit","-m","appendUsers")
+	cmd = exec.Command("git", "commit", "-m", "appendUsers")
 	cmd.Output()
 	return nil
 }
 
-
-
-func main()  {
-	strData,err1 := git.GetsUserEmail()
+func main() {
+	strData, err1 := git.GetsUserEmail()
 	if err1 != nil {
 		panic(err1)
 	}
-	PrintToFileData(PATHS,strData)
+	PrintToFileData(PATHS, strData)
 
-	strData,err1 = git.GetsUserName()
+	strData, err1 = git.GetsUserName()
 	if err1 != nil {
 		panic(err1)
 	}
-	PrintToFileData(PATHS,strData)
-	fmt.Print("wg")
-} 
+	PrintToFileData(PATHS, strData)
+
+}
