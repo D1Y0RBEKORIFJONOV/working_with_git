05561a533d9c55b69f8b03c9108c8d5b7117119a
Diyorbek <D1Y0RBEKORIFJONOV>
2024-04-25 19:02:42 +0500

    appendUsers

--- cmd/main.go
+++ cmd/main.go
@@ -9,14 +9,15 @@ import (
 
 
 func PrintToFile(fileName string,data string) error {
-	err := os.WriteFile(fileName,[]byte(data),777)
+	err := os.WriteFile(fileName,[]byte(data),0344)
 	if err != nil {
 		return err
 	}
 
 	cmd := exec.Command("git","add",".")
 	cmd.Output()
-	
+	cmd = exec.Command("git","commit","-m","appendUsers")
+	cmd.Output()
 	return nil
 }
 
@@ -25,4 +26,9 @@ func PrintToFile(fileName string,data string) error {
 
 
 func main()  {
+	str,err := git.GetUserName()
+	if err != nil {
+		panic(err)
+	}
+	PrintToFile("/home/diyorbek/go/src/working_with_git/file.txt",str)
 }
